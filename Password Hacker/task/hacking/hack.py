0639d528363a054848e8ec3edc06e4cd6e517d3e
ivan.silva <ivan.elisandro@gmail.com>
2021-02-04 20:23:00 -0300

    CHANGED: Implementation of Stage 2 of the Password Challenge project. Created during my studies using JetBrains Academy.
    This stage changes the script to receive 2 parameters (ip and port), connects to the host and tries to discover the password by brute force.
    
    Important files:
    Password Hacker/task/hacking/hack.py

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,20 +1,49 @@
 import socket
 import argparse
+import itertools
+import string
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip_address')
 parser.add_argument('port')
-parser.add_argument('message')
 
 args = parser.parse_args()
 
-with socket.socket() as client_socket:
-    address = (args.ip_address, int(args.port))
 
-    client_socket.connect(address)
+class Kraken:
+    _characters = tuple(string.ascii_lowercase)
+    _numbers = tuple(string.digits)
+    _all_values = tuple(itertools.chain(_characters, _numbers))
+    _current_list = _all_values[:]
 
-    data = args.message.encode()
-    client_socket.send(data)
-    response = client_socket.recv(1024)
+    def __init__(self):
+        self.address = (args.ip_address, int(args.port))
 
-    print(response.decode())
+    def next_attempt(self):
+        while True:
+            for text in self._current_list:
+                yield text
+            self._current_list = [a + b for a, b in itertools.product(self._current_list[:], self._all_values)]
+
+    def bite(self):
+        with socket.socket() as client_socket:
+            client_socket.connect(self.address)
+
+            for word in self.next_attempt():
+                data = word.encode()
+                client_socket.send(data)
+                response = client_socket.recv(1024)
+                decoded_response = response.decode()
+
+                if decoded_response == "Wrong password!":
+                    continue
+                elif decoded_response == "Connection success!":
+                    print(word)
+                    break
+                elif decoded_response == "Too many attempts":
+                    break
+
+
+kraken = Kraken()
+kraken.bite()
